df7842bdd1a7319d6abfe2f2567824fa93ec46f2
Your Name <tewedaj13@gmail.com>
2022-07-19 14:01:31 -0700

    once new line remaining

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,7 @@
 This module defines a Square class and initialise its size
 """
 
-#this is the square implementation function
+
 class Square:
     """Square implementation
     """
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,6 +5,7 @@ This module defines a Square class
 Its implements value and type checks for its attributes
 """
 
+
 class Square:
     """Square implementation
     """
