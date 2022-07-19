ef1d24c3a6616bac430134fa4bf6fce9a83b51dc
Your Name <tewedaj13@gmail.com>
2022-07-19 13:01:19 -0700

    updated some indentation and stuff

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-""" this is a great way to find the index of a value with in
-an array
-"""
 def element_at(my_list, idx):
     return my_list[idx] if (0 <= idx < len(my_list)) else None
