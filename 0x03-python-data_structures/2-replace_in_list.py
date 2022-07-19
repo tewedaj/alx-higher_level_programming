9697999a7e90e2a3d94cd1059b659155eb1c4b4f
Your Name <tewedaj13@gmail.com>
2022-07-19 13:38:20 -0700

    all changes have been made, betty should not bother me now

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-this is a good way to replace a value in a list using
-the index given in the second parameter
-"""
 def replace_in_list(my_list, idx, element):
     if (0 <= idx < len(my_list)):
         my_list[idx] = element
